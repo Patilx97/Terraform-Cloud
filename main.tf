02fcaf7025672a29842cb427d82d1d3f7c3588eb
stark303test <86427704+stark303test@users.noreply.github.com>
2024-07-04 13:35:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,29 +6,36 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region_name
+  region     = var.region_name
 }
-variable "access_key"{
-   type = string
+
+variable "access_key" {
+  type = string
 }
-variable "secret_key"{
-   type = string
+
+variable "secret_key" {
+  type = string
 }
-variable "region_name"{
-   type = string
-   default = "us-east-1"
+
+variable "region_name" {
+  type    = string
+  default = "us-east-1"
 }
+
 resource "aws_instance" "myec2" {
   ami                    = "ami-06c68f701d8090592"
   instance_type          = "t2.micro"
-  subnet_id     = "subnet-01064b369aa9021eb"
+  subnet_id              = "subnet-01064b369aa9021eb"
+  security_groups        = [aws_security_group.allow_ssh.name]
   tags = {
     Name = "terraform1"
   }
 }
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -39,3 +46,11 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
